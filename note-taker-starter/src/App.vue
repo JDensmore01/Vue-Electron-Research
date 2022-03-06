25d2a5c97d00e77e0484e4bf77a189e41977b035
Brian Aylward <brian@Brians-MacBook-Pro.local>
2022-03-06 13:48:04 -0400

    Updated Styles

--- note-taker-starter/src/App.vue
+++ note-taker-starter/src/App.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Abel&display=swap");
 *,
 *::before,
 *::after {
@@ -20,22 +21,29 @@ export default {
   box-sizing: border-box;
 }
 body {
-  background: #0f2027; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to right,
-    #2c5364,
-    #203a43,
-    #0f2027
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to right,
-    #2c5364,
-    #203a43,
-    #0f2027
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  /* background: #0f2027;
+  background: -webkit-linear-gradient(to right, #2c5364, #203a43, #0f2027);
+  background: linear-gradient(to right, #2c5364, #203a43, #0f2027); */
+
+  /* background: #2193b0;
+  background: -webkit-linear-gradient(to right, #6dd5ed, #2193b0);
+  background: linear-gradient(to right, #6dd5ed, #2193b0); */
+
+  /* background: #a8c0ff;
+  background: -webkit-linear-gradient(to right, #3f2b96, #a8c0ff);
+  background: linear-gradient(to right, #3f2b96, #a8c0ff); */
+
+  /* background: #000000;
+  background: -webkit-linear-gradient(to right, #0f9b0f, #000000);
+  background: linear-gradient(to right, #0f9b0f, #000000); */
+
+  background: #780206;
+  background: -webkit-linear-gradient(to right, #061161, #780206);
+  background: linear-gradient(to right, #061161, #780206);
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: "Abel", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- note-taker-starter/src/components/Notes.vue
+++ note-taker-starter/src/components/Notes.vue
@@ -1,37 +1,35 @@
 <template>
-  <div class="note text-start">
+  <div class="note">
     <!-- Add a new Note Button -->
-    <button class="mt-4 mx-3 rounded-pill">
-      <i class="fas fa-plus fs-4"></i>
+    <button class="my-4 mx-lg-3 rounded-pill">
+      <i class="fas fa-plus fs-3"></i>
     </button>
-
     <!-- Main note area -->
-    <div class="note-header">
-      <div class="noteList">
-        <h3>My Notes</h3>
-        <ul>
+    <div class="note-header ps-2 my-4">
+      <div class="noteList me-lg-5 p-lg-3 mb-2">
+        <h3 class="text-center">My Notes</h3>
+        <ul class="note-item-list mx-auto">
           <li
-            class="d-flex align-items-center py-2 border-bottom fw-bold justify-content-between"
+            class="note-item d-flex align-items-center py-2 border-bottom fw-bold justify-content-between"
           >
-            <span class="btn text-white">Title</span>
-            <button class="btn bg-danger rounded-pill p-0 px-3">X</button>
+            <span class="btn text-white text-start">Title</span>
+            <button class="btn bg-danger">X</button>
           </li>
         </ul>
       </div>
-      <div class="note-display">
+      <div class="note-display text-center">
         <h4>Title</h4>
         <p>Description</p>
         <button class="me-3">Edit</button>
         <button>Close Note</button>
       </div>
     </div>
-
     <!-- Note creation/edit form -->
     <div class="noteForm text-center">
       <label class="form-label">Title:</label><br />
       <input type="text" class="form-control" minlength="2" />
       <label class="form-label">Description:</label>
-      <textarea ref="noteDesc" class="form-control"></textarea>
+      <textarea class="form-control"></textarea>
       <button class="mt-3 me-3">Save</button>
     </div>
   </div>
@@ -47,27 +45,18 @@ export default {
 <style scoped>
 .note-header {
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
 }
 .noteForm {
-  border-right: 1px solid #000;
   padding: 0 2rem 2rem 2 rem;
   /* opacity: 0; */
   transition: 200ms ease;
+  max-width: 400px;
+  margin: 0 auto;
 }
 .noteForm.display {
-  opacity: 1;
-}
-.note-display {
-  /* opacity: 0; */
-  transition: 200ms ease;
-  text-align: start;
-  padding: 2rem;
-  max-width: 320px;
-  width: 320px;
-  word-wrap: break-word;
-}
-.note-display.display {
   opacity: 1;
 }
 .noteForm * {
@@ -78,11 +67,23 @@ export default {
   padding: 0;
 }
 .noteList {
-  padding: 2rem;
-  width: 50%;
+  min-width: 300px;
   text-align: start;
 }
-
+.note-item-list {
+  max-width: 350px;
+}
+.note-display {
+  /* opacity: 0; */
+  transition: 200ms ease;
+  text-align: start;
+  max-width: 450px;
+  width: 450px;
+  word-wrap: break-word;
+}
+.note-display.display {
+  opacity: 1;
+}
 input[type="text"],
 textarea {
   width: 300px;
